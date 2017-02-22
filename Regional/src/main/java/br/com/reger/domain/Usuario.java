441b08bc34bcd39d195917195cd630b8ee0f1f1c
Leonardo Simonetti <leosimonetti@hotmail.com>
2017-02-22 19:16:03 -0300

    Testes

--- Regional/src/main/java/br/com/reger/domain/Usuario.java
+++ Regional/src/main/java/br/com/reger/domain/Usuario.java
@@ -3,28 +3,13 @@ package br.com.reger.domain;
 import java.io.Serializable;
 import java.sql.Date;
 
-import javax.annotation.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
-
-@ManagedBean(value = "Usuario")
-@ViewScoped
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	@ManagedProperty("#{id}")
 	private Long id;
-	
-	@ManagedProperty("#{nomeUsuario}")
 	private String nomeUsuario;
-	
-	@ManagedProperty("#{senha}")
 	private String senha;
-	
-	@ManagedProperty("#{ultimoLogin}")
 	private Date ultimoLogin;
-	
-	@ManagedProperty("#{superUsuario}")
 	private Boolean superUsuario;
 
 	public Long getId() {
